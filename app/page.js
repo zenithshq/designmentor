1d0856d6c54e484bde317aeb34f08b1d117d4b7e
aryannn053 <aryannn053@gmail.com>
2024-07-16 10:43:02 +0530

    trigger deployement

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { Introduction } from "../components/Introduction"
 
 import './app.css';
 
+
 export default function Home() {
   return (
     <div className="mx-auto md:max-w-lg lg:max-w-7xl px-14">
